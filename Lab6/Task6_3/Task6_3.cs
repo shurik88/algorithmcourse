a55691e83f24ed719347309b6abe971f2e34d69f
Александр Будыльский <budylsky@adeptik.com>
2017-04-02 20:26:21 +0300

    Task 6_3

---
+++ Lab6/Task6_3/Task6_3.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6.Task6_3
+{
+    public class Task6_3
+    {
+        public static void Main(string[] args)
+        {
+            
+            try
+            {
+                var content = File.ReadAllLines("input.txt");
+                if (Int32.Parse(content[0]) == 0)
+                {
+                    File.WriteAllText("output.txt", "0");
+                    return;
+                }
+                var root = ParseNode(1, content);
+                File.WriteAllText("output.txt", root.Height.ToString());
+            }
+            catch(Exception e)
+            {
+                File.WriteAllText("output.txt", e.Message);
+            }
+            
+
+        }
+
+        public static Node ParseNode(int rootIndex, string[] treeDef)
+        {
+            var nodeDef = treeDef[rootIndex].Split(new[] { ' ' }).Select(x => Int32.Parse(x)).ToArray();
+            var node = new Node { Key = nodeDef[0] };
+            if (nodeDef[1] != 0)
+                node.Left = ParseNode(nodeDef[1], treeDef);
+            if (nodeDef[2] != 0)
+                node.Right = ParseNode(nodeDef[2], treeDef);
+            return node;
+        }
+
+        public class Node
+        {
+            public Node Parent { get; set; }
+
+            public int Key { get; set; }
+
+            public Node Left { get; set; }
+
+            public Node Right { get; set; }
+
+            public int Height
+            {
+                get
+                {
+                    var left = Left != null ? Left.Height : 0;
+                    var right = Right != null ? Right.Height : 0;
+                    return Math.Max(left, right) + 1;
+                }
+            }
+
+            //public static Node Insert(Node to, Node inserted)
+            //{
+            //    if (to == null)
+            //        return inserted;
+            //    if (to.Key > inserted.Key)
+            //    {
+            //        to.Left = Insert(to.Left, inserted);
+            //        return to.Left;
+            //    }
+            //    else
+            //    {
+            //        to.Right = Insert(to.Right, inserted);
+            //        return to.Right;
+            //    }
+
+            //}
+        }
+    }
+}
---
+++ TestLab/UnitTest6_3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab6.Task6_3;
+
+namespace TestLab
+{
+    /// <summary>
+    /// Summary description for UnitTest6_3
+    /// </summary>
+    [TestClass]
+    public class UnitTest6_3
+    {
+        
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestBinaryTreeSize()
+        {
+            const int size = 118291;
+            var treeDef = GetLeftTreeDefinition(size, 1000);
+            var tree = Task6_3.ParseNode(1, treeDef);
+            Assert.AreEqual(size, tree.Height);
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        private string[] GetLeftTreeDefinition(int size, int initKey)
+        {
+            var treeDef = new string[size + 1];
+            treeDef[0] = size.ToString();
+            for(var i = 1; i <= size; ++i)
+            {
+                treeDef[i] = string.Join(" ", initKey - i + 1, i == size ? 0 : i + 1, 0);
+            }
+            return treeDef;
+        }
+    }
+}
