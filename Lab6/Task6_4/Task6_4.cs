27afdd025f1e32a59ee812f3a718d7231ee5de5b
Александр Будыльский <budylsky@adeptik.com>
2017-04-08 13:14:53 +0300

    Решение задачи 6_4

--- Lab6/Task6_4/Task6_4.cs
+++ Lab6/Task6_4/Task6_4.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab6.Task6_4
 {
@@ -13,24 +11,115 @@ namespace Lab6.Task6_4
         {
             var content = File.ReadAllLines("input.txt").ToList();
 
-            var arraySize = Int32.Parse(content[0]);
-            if(arraySize == 0)
+            _fileSize = Int32.Parse(content[0]);
+            if(_fileSize == 0)
             {
                 File.WriteAllText("output.txt", "0");
                 return;
             }
 
-            var treeDef = content.GetRange(0, arraySize + 1);
-            var commands = content[arraySize + 2];
+            _treeDef = content.GetRange(0, _fileSize + 1).ToArray();
+            _array = Enumerable.Repeat(NullValue, (int)Math.Pow(2, Math.Min(25, _fileSize) - 1)).ToArray();
+            ParseTree(0, 1);
+            var commands = content[_fileSize + 2].Split(new[] { ' '}).Select(x => Int32.Parse(x));
+            using (var output = new StreamWriter("output.txt"))
+            {
+                try
+                {
+                    foreach (var com in commands)
+                    {
+                        var indexToDelete = GetIndex(com);
+                        if (indexToDelete == -1)
+                            output.WriteLine(_size.ToString());
+                        else
+                        {
+                            DeleteSubTree(indexToDelete);
+                            output.WriteLine(_size.ToString());
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+                    var st = new StackTrace(e, true);
+
+                    // Get the top stack frame
+                    var frame = st.GetFrame(0);
+                    // Get the line number from the stack frame
+                    //var line = frame.GetMethod().Name.GetFileLineNumber();
+                    output.WriteLine(frame.GetMethod().Name);
+                }
+                
+            }
+                
+
+            //var treeToDelete = new BinaryTreeToDelete(treeDef, 25, 1000000000 + 1);
+        }
+
+        private static int _fileSize = 0;
+
+        const int NullValue = 1000000001;
+
+        private static string[] _treeDef;
+
+        private static int[] _array; //=Enumerable.Repeat(NullValue, (int)Math.Pow(2, 25) - 1).ToArray(); //new int[(int)Math.Pow(2, 25) - 1];//
+
+        private static int _size = 0;
+
+        private static void ParseTree(int indexToInsert, int treeDefIndex)
+        {
+            var nodeDef = _treeDef[treeDefIndex].Split(new[] { ' ' });
+            var left = Int32.Parse(nodeDef[1]);
+            var right = Int32.Parse(nodeDef[2]);
+
+            _array[indexToInsert] = Int32.Parse(nodeDef[0]);
+            _size++;
+
+            if (left != 0)
+                ParseTree(indexToInsert * 2 + 1, left);
+            if (right != 0)
+                ParseTree(indexToInsert * 2 + 2, right);
+        }
+
+        private static int GetIndex(int key)
+        {
+            var index = SearchIndex(key, 0);
+            if (_array[index] != key)
+                return -1;
+            return index;
 
         }
 
-        public class BinaryTreeToDelete
+        private static void DeleteSubTree(int index)
         {
-            public BinaryTreeToDelete(int maxHeight, int maxValue)
-            {
+            _size -= GetSubTreeCount(index);
+            _array[index] = NullValue;            
+        }
 
+        private static int SearchIndex(int key, int indexToStart)
+        {
+            if (_array[indexToStart] == NullValue || _array[indexToStart] == key)
+                return indexToStart;
+            if (_array[indexToStart] < key)
+            {
+                if (indexToStart * 2 + 2 >= _array.Length)
+                    return indexToStart;
+                return SearchIndex(key, indexToStart * 2 + 2);
             }
+            else
+            {
+                if (indexToStart * 2 + 1 >= _array.Length)
+                    return indexToStart;
+                return SearchIndex(key, indexToStart * 2 + 1);
+            }
+        }
+
+
+        private static int GetSubTreeCount(int index)
+        {
+            if (index >= _array.Length || _array[index] == NullValue)
+                return 0;
+            else
+                return GetSubTreeCount(index * 2 + 1) + GetSubTreeCount(index * 2 + 2) + 1;
         }
     }
 }
