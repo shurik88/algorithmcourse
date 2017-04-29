08acb585c58284b163ac54cc73fa5009b1f9918d
Александр Будыльский <budylsky@adeptik.com>
2017-04-29 10:07:33 +0300

    Task6_2, Task6_5

---
+++ Lab6/Task6_2/Task6_2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6.Task6_2
+{
+    public class Task6_2
+    {
+        public static void Main(string[] args)
+        {
+            var content = File.ReadAllText("input.txt");
+            var data = content.Split(new[] {' '});
+            if (data.Length != 2)
+                throw new ArgumentException("Invalid file format");
+
+            var n = Int32.Parse(data[0]);
+            var a = double.Parse(data[1], CultureInfo.InvariantCulture);
+            if (n < 3)
+                throw new ArgumentException("Invalid N");
+            if (n == 3)
+            {
+                if(n < 2)
+                    File.WriteAllText("output.txt", GetNext(a, 0).ToString(CultureInfo.InvariantCulture));
+                else
+                {
+                    File.WriteAllText("output.txt", "0.0");
+                }
+                return;
+            }
+
+            const double E = 0.00000000001;
+            var up = a;
+            var down = 0.0;
+            while(true)
+            {
+                var h2 = (up + down) / 2.0;
+                var numbers = new double[n];
+                numbers[0] = a;
+                numbers[1] = h2;
+                for (var j = 2; j < n; ++j)
+                {
+                    numbers[j] = 2 * (numbers[j - 1] + 1) - numbers[j - 2];
+                }
+                
+                //Console.WriteLine($" {h2}: {string.Join(" ", numbers)}");
+                var count = numbers.Count(x => x <= 0 || Math.Abs(x) < E);
+                if (count == 1 && Math.Abs(numbers.First(x => x <= 0 || Math.Abs(x) < E)) <= E)
+                {
+                    File.WriteAllText("output.txt", numbers[n - 1].ToString(CultureInfo.InvariantCulture));
+                    return;
+                }
+                if (count == 0)
+                {
+                    up = h2;
+                }
+                else
+                {
+                    down = h2;
+                }
+            }
+
+        }
+
+        private static double GetNext(double hi0, double hi1)
+        {
+            return 2 * (hi1 + 1) - hi0;
+        }
+    }
+}
---
+++ Lab6/Task6_5/Task6_5.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6.Task6_5
+{
+    public class Task6_5
+    {
+        public static void Main(string[] args)
+        {
+            var content = File.ReadAllLines("input.txt");
+
+            if (content.Length < 3)
+            {
+                File.WriteAllText("output.txt", "YES");
+                return;
+            }
+            var first = content[1].Split(new[] {' '});
+
+            var stack = new Stack<Info>();
+            stack.Push(new Info { Key = int.Parse(first[0]), Left = int.Parse(first[1]), Right = int.Parse(first[2])});
+            while (stack.Any())
+            {
+                var elem = stack.Pop();
+                if (elem.Left != 0)
+                {
+                    var left = content[elem.Left].Split(new[] { ' ' });
+                    var leftKey = int.Parse(left[0]);
+                    if ( leftKey >= elem.Key || (elem.MaxRight.HasValue && elem.MaxRight.Value <= leftKey) || (elem.MinLeft.HasValue && elem.MinLeft.Value >= leftKey))
+                    {
+                        File.WriteAllText("output.txt", "NO");
+                        return;
+                    }
+                    stack.Push(new Info
+                    {
+                        Key = leftKey,
+                        Left = int.Parse(left[1]),
+                        Right = int.Parse(left[2]),
+                        MaxRight = elem.Key,
+                        MinLeft = elem.MinLeft
+                    });
+                }
+                if (elem.Right != 0)
+                {
+                    var right = content[elem.Right].Split(new[] { ' ' });
+                    var rightKey = int.Parse(right[0]);
+                    if (rightKey <= elem.Key || (elem.MaxRight.HasValue && elem.MaxRight.Value <= rightKey) || (elem.MinLeft.HasValue && elem.MinLeft.Value >= rightKey))
+                    {
+                        File.WriteAllText("output.txt", "NO");
+                        return;
+                    }
+                    stack.Push(new Info
+                    {
+                        Key = rightKey,
+                        Left = int.Parse(right[1]),
+                        Right = int.Parse(right[2]),
+                        MinLeft = elem.Key,
+                        MaxRight = elem.MaxRight
+                    });
+                }
+            }
+
+            File.WriteAllText("output.txt", "YES");
+        }
+
+        public struct Info
+        {
+            public int Key { get; set; }
+
+            public int Left { get; set; }
+
+            public int Right { get; set; }
+
+            public int? MaxRight { get; set; }
+
+            public int? MinLeft { get; set; }
+        }
+    }
+}
